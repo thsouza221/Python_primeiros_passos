79875ccad90866bbc2ea68ca7c8f2a1263b79300
thsouza221 <thsouza221@gmail.com>
2021-10-22 19:50:34 -0300

    banco

--- ageindays.py
+++ ageindays.py
@@ -2,12 +2,13 @@
 # Inputs:
 idade = int(input('Digite sua idade em dias'))
 # Função:
-ano = idade/365
+ano = idade//365
 resto1 = idade % 365
 print('%.0f ano(s)' %(ano))
+mes = 0
+dia = 0
 if resto1 != 0:
-    mes = resto1 / 30
+    mes = resto1 // 30
     dia = resto1 % 30
-    print('%.0f mes(es)' %(mes))
-    if dia != 0:
-        print(f'{dia} dia(s)')
+print('%.0f mes(es)' %(mes))
+print(f'{dia} dia(s)')
---
+++ banco.py
@@ -0,0 +1,37 @@
+money = float(input('Digite o valor do dinheiro: '))
+n100 = money//100
+print('NOTAS:')
+print('%.0f nota(s) de R$ 100.00' %(n100))
+montante = money % 100
+n50 = montante//50
+print('%.0f nota(s) de R$50.00' %(n50))
+montante = montante % 50
+n20 = montante//20
+print('%.0f nota(s) de R$20.00' %(n20))
+montante = montante % 20
+n10 = montante//10
+print('%.0f nota(s) de R$10.00' %(n10))
+montante = montante % 10
+n5 = montante//5
+print('%.0f nota(s) de R$5.00' %(n5))
+montante = montante % 5
+n2 = montante//2
+print('%.0f nota(s) de R$2.00' %(n2))
+print('MOEDAS:')
+montante = (montante % 2)*100
+n1 = montante//100
+print('%.0f moedas(s) de R$1.00' %(n1))
+montante = montante % 100
+n05 = montante//50
+print('%.0f moedas(s) de R$0.50' %(n05))
+montante = montante % 50
+n025 = montante//250.83
+print('%.0f moedas(s) de R$0.25' %(n025))
+montante = montante % 25
+n010 = montante//10
+print('%.0f moedas(s) de R$0.10' %(n010))
+montante = montante % 10
+n005 = montante//5
+print('%.0f moedas(s) de R$0.05' %(n005))
+n001= montante % 5
+print('%.0f moedas(s) de R$0.01' %(n001))
